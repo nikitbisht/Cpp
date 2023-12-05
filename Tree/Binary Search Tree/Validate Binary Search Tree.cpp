e8b260caf4d3c80b8a5018a3dc53cb25f6f226d6
Nikit Singh Bisht <135353429+nikitbisht@users.noreply.github.com>
2023-12-05 21:28:23 +0530

    Medium

---
+++ Tree/Binary Search Tree/Validate Binary Search Tree.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    bool solve(TreeNode* root, int mini,int maxi){
+        //base case
+        if(!root)   return true;
+
+        if(root->val >= mini && root->val <= maxi){
+            bool left = solve(root->left,mini,root->val);
+            bool right= solve(root->right,root->val,maxi);
+            return left && right;
+        }
+        return false;
+    }
+public:
+    bool isValidBST(TreeNode* root) {
+        return solve(root,INT_MIN,INT_MAX);
+    }
+};
+/*1 Approch :- inorder traversal of Valid binary Search tree is Shorted so we chcek its sorted or not. 
+Tc:- for inorder traversal O(n) + check sorted or not O(n)= O(n)
+Sc:-  O(n) stored element 
+
+2 Approch:- As we know that left tree data < root > right tree data.
+Tc:- O(n)
+Sc:- O(h)   O(N) for skwee tree
+*/
